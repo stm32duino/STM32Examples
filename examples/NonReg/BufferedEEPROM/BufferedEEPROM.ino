dbf87f9182ded848fee183508f2f5e3412488d04
Frederic Pillon <frederic.pillon@st.com>
2020-09-28 10:04:40 +0200

    BufferedEEPROM build on MCU with EEPROM
    Allows to build for CI and warn user.

--- examples/NonReg/BufferedEEPROM/BufferedEEPROM.ino
+++ examples/NonReg/BufferedEEPROM/BufferedEEPROM.ino
@@ -1,51 +1,58 @@
+#if defined(DATA_EEPROM_BASE)
+#warning "STM32 devices have an integrated EEPROM. No buffered API available."
+
+void setup() {
+}
+
+void loop() {
+}
+
+#else
 #include <EEPROM.h>
 
 /**
- * Most STM32 devices don't have an integrated EEPROM.
- * To emulate a EEPROM, the STM32 Arduino core emulated
- * the operation of an EEPROM with the help of the embedded
- * flash.
- *
- * Writing to a flash is very expensive operation, since a
- * whole flash page needs to be written, even if you only
- * want to access the flash byte-wise.
- *
- * The STM32 Arduino core provides a buffered access API
- * to the emulated EEPROM. The library has allocated the
- * buffer even if you don't use the buffered API, so
- * it's strongly suggested to use the buffered API anyhow.
- */
+   Most STM32 devices don't have an integrated EEPROM.
+   To emulate a EEPROM, the STM32 Arduino core emulated
+   the operation of an EEPROM with the help of the embedded
+   flash.
 
-#if defined(DATA_EEPROM_BASE)
-#error "STM32 devices have an integrated EEPROM. No buffered API available."
-#endif
+   Writing to a flash is very expensive operation, since a
+   whole flash page needs to be written, even if you only
+   want to access the flash byte-wise.
+
+   The STM32 Arduino core provides a buffered access API
+   to the emulated EEPROM. The library has allocated the
+   buffer even if you don't use the buffered API, so
+   it's strongly suggested to use the buffered API anyhow.
+*/
 
 #define DATA_LENGTH E2END
 
 void setup() {
-	Serial.begin(115200);
+  Serial.begin(115200);
 }
 
 void loop() {
-	// Fill the EEPROM buffer in memory with data
-	for (uint16_t i = 0; i < DATA_LENGTH; i++) {
-		eeprom_buffered_write_byte(i, i % 256);
-	}
+  // Fill the EEPROM buffer in memory with data
+  for (uint16_t i = 0; i < DATA_LENGTH; i++) {
+    eeprom_buffered_write_byte(i, i % 256);
+  }
 
-	// Copy the data from the buffer to the flash
-	eeprom_buffer_flush();
+  // Copy the data from the buffer to the flash
+  eeprom_buffer_flush();
 
-	// Clear the buffer for demonstration purpose
-	for (uint16_t i = 0; i < DATA_LENGTH; i++) {
-		eeprom_buffered_write_byte(i, 0);
-	}
+  // Clear the buffer for demonstration purpose
+  for (uint16_t i = 0; i < DATA_LENGTH; i++) {
+    eeprom_buffered_write_byte(i, 0);
+  }
 
-	// Print the 254th byte of the current buffer (should be 0)
-	Serial.println(eeprom_buffered_read_byte(254));
+  // Print the 254th byte of the current buffer (should be 0)
+  Serial.println(eeprom_buffered_read_byte(254));
 
-	// Copy the data from the flash to the buffer
-	eeprom_buffer_fill();
+  // Copy the data from the flash to the buffer
+  eeprom_buffer_fill();
 
-	// Print the 254th byte of the current buffer (should be 254)
-	Serial.println(eeprom_buffered_read_byte(254));
+  // Print the 254th byte of the current buffer (should be 254)
+  Serial.println(eeprom_buffered_read_byte(254));
 }
+#endif
