3667312388d33fd08993bb9fc819db4613f463ab
Alexandre Bourdiol <alexandre.bourdiol@st.com>
2021-12-15 10:16:26 +0100

    HardwareTimer: NonReg: Fix spelling
    Signed-off-by: Alexandre Bourdiol <alexandre.bourdiol@st.com>

--- examples/NonReg/HardwareTimer/HardwareTimer_OutputInput_test/HardwareTimer_OutputInput_test.ino
+++ examples/NonReg/HardwareTimer/HardwareTimer_OutputInput_test/HardwareTimer_OutputInput_test.ino
@@ -8,9 +8,9 @@
   Designed to work on Nucleo_L476RG
   TIM1 ch1N and ch2 are used for output generation (TIM1_CH1N_PIN, and TIM1_CH2_PIN).
   It is important that both channel are on the same timer, in order to check influence on channel dependant API.
-  Management of interruption is done with variable incrementation.
-  TIM8 ch1/ch2 are used to measure frequence and duty cycle of TIM1_CH1N output generated signal
-  TIM8 ch3/ch4 are used to measure frequence and duty cycle of TIM1_CH2 output generated signal
+  Management of interruption is done with variable increment.
+  TIM8 ch1/ch2 are used to measure frequency and duty cycle of TIM1_CH1N output generated signal
+  TIM8 ch3/ch4 are used to measure frequency and duty cycle of TIM1_CH2 output generated signal
 
   Regular channel and complementary channel (TIM1_CH1N) are tested.
 
@@ -61,7 +61,7 @@
 #define OUTPUT_DUTY1 20        // percentage
 #define OUTPUT_DUTY2 30        // percentage
 
-#define TEMPO_BEFORE_MEASUREMENT 500 // milisecondes
+#define TEMPO_BEFORE_MEASUREMENT 500 // millisecondes
 #define TOLERANCE 10                 // Percentage
 #define EXPECTED_INTERRUPT_COUNT ((TEMPO_BEFORE_MEASUREMENT * OUTPUT_FREQUENCY) / 1000)
 
